453c7b6a719b56e0c1d367348b2136116be79d16
Olivia Chen <oyc4@duke.edu>
2023-09-18 18:58:52 -0400

    create model + controller + routes + seed for items

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+puts 'seeding'
+
+item1 = Item.create(name: "item1", description: "pog", item_type: "sticker", rarity: 0)
+item2 = Item.create(name: "item2", description: "pog1", item_type: "sticker", rarity: 1)
+item3 = Item.create(name: "item3", description: "pog3", item_type: "clothing", rarity: 0)
+
+puts 'successfully seeded'
