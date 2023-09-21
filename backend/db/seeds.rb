452dbd8a6b1810ec6414f10ac80162f4385cc1bb
25dlee <dasol.lee@duke.edu>
2023-09-21 15:33:29 -0400

    added tools maybe

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -14,3 +14,6 @@ item2 = Item.create(name: "item2", description: "pog1", item_type: "sticker", ra
 item3 = Item.create(name: "item3", description: "pog3", item_type: "clothing", rarity: 0)
 
 puts 'successfully seeded'
+
+scissor = Tool.create(name: "scissor", description: "it cuts materials", globalCount: 0)
+hammer = Tool.create(name: "hammer", description: "it slams stuff", globalCount: 0)
