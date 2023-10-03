ec43bc7d01a3255960b8f42bb46dbb4204caca19
Kelvin Bueno <kelvinbueno@Kelvins-MacBook-Pro.local>
2023-10-03 12:04:40 -0400

    added frontend kaboom game

---
+++ FrontEnd/Sprite-Grab-and-Drop/utils/loader.js
@@ -0,0 +1,42 @@
+export const load = {
+    // font: () => {
+    //     // can load in font here
+    //     // loadFont("")
+    // },
+    assets: () => {
+        loadSprite("colabdoors","./assets/colabsdoorsmall.jpeg")
+        loadSprite("colablogo", "./assets/colablogo.png")
+        loadSprite("characterSprite", "./assets/sprite.png")
+        loadSprite("colabBackground", "./assets/colabbackground.png")
+        loadSprite("blackpixelborder", "./assets/blackpixelborder.png")
+        loadSprite("PLAalert", "./assets/PLAalert.png")
+        loadSprite("noItems", "./assets/noItems.png")
+        loadSprite("cricutAlertBox", "./assets/machine1AlertBox.png")
+        loadSprite("cricut","./assets/cricut-transparent.png")
+        loadSprite("scissors", "./assets/scissor-pixel.png")
+        loadSprite("scissorsAlert", "./assets/scissorsAlert.png")
+        loadSprite("paperAlert", "./assets/paperAlertBox.png")
+        loadSprite("paper", "./assets/paper.PNG")
+        loadSprite("woodAlert", "./assets/colablogo.png")
+        loadSprite("wood", "./assets/wood.PNG")
+        loadSprite("neededAlert", "./assets/neededAlert.png")
+        loadSprite("neededBlueprintAlert", "./assets/neededBlueprintAlert.png")
+        loadSprite("3DBenchy", "./assets/benchy.png")
+        loadSprite("3DBenchyAlert", "./assets/benchyAlert.png")
+        loadSprite("hammer", "./assets/hammer.PNG")
+        loadSprite("yarn", "./assets/yarn.png")
+        loadSprite("hammerAlert", "./assets/hammer.png")
+        loadSprite("yarnAlert", "./assets/yarn.png")
+
+        loadSprite("solder", "./assets/solder.PNG")
+        loadSprite("woodArtMario", "./assets/woodartmario.PNG")
+
+
+
+
+
+
+        
+    }
+
+}
