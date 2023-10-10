2b12e03b82a2b1e80a3dfde1b06a385870b91540
Anni Yan <ay114@duke.edu>
2023-10-10 14:17:27 -0400

    updated, pls read README in frontend/ for more info

--- FrontEnd/server.js
+++ FrontEnd/server.js
@@ -1,49 +1,22 @@
 'use strict';
 
-// const reload = require('reload')
 const express = require('express');
- 
-// Constants
-const PORT = 3000;
-const HOST = '0.0.0.0';
  
 // App
 const app = express();
-// app.get('/', (req, res) => {
-//   res.send('<html><body><p style="font-size: 24px; font-weight: bold;">Hello, Co-Lab!! 🕺</p></body></html>');
-// });
  
 app.get('/', (req, res) => {
   // This is an example HTML structure, adapt it to your needs
-  res.send(`
-    <html>
-      <head>
-        <title>Kaboom Game</title>
-      </head>
-      <body>
-        <style>
-        * {
-          margin: 0;
-        }
-        html,
-        body {
-          width: 100%;
-          height: 100%;
-          overflow: hidden;
-        }
-        canvas {
-          display: block;
-        }
-      </style>
-        <h1> hello main </h1>
-        <script type="module" src="main.js"></script>
-    </html>
-  `);
+  res.sendFile(__dirname + "/index.html");
 });
-
-app.listen(PORT, HOST, () => {
-  console.log(`Running on http://${HOST}:${PORT}`);
+ 
+app.get('/main.js', (req, res) => {
+  // This is an example HTML structure, adapt it to your needs
+  res.sendFile(__dirname + "/main.js");
 });
+ 
+app.use('/utils', express.static('utils'))
+app.use('/libs', express.static('libs'))
+app.use('/assets', express.static('assets'))
 
-// reload(express);
-
+module.exports = app
