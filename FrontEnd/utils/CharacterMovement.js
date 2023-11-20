08c8186ad756ccc2cf5002ecfe0154f7107521ce
Alex <akp42@duke.edu>
2023-11-20 10:59:13 -0500

    added background

--- FrontEnd/utils/CharacterMovement.js
+++ FrontEnd/utils/CharacterMovement.js
@@ -1704,8 +1704,38 @@ class CharacterMovement {
         inPocket.push(newItem);
         itemsInPocket++;
       } else {
+        let alertText = "Remove items from pocket to select from vending machine";
+
+        add([
+          "alertPop",
+          text(alertText, {
+            // optional object
+            size: 24,
+            outline: 4,
+            color: (0, 0, 0),
+            // can specify font here,
+          }),
+          area(),
+          anchor("center"),
+          pos(500+25, 500-300),
+          z(20),
+          // scale(.5)
+        ]);
+        add([
+          rect(500+200+200,50),
+          area(),
+          anchor("center"),
+          pos(500+25, 500-300),
+          z(19),
+          color(242, 140, 40),
+          "alertPop"
+
+        ])
+
+        setTimeout(() => {
+          destroyAll("alertPop");
+        }, 2000);
         // shake(5);
-        alert("Remove items from pocket to select from vending machine");
       }
     }
     
@@ -1913,10 +1943,20 @@ class CharacterMovement {
           z(20),
           // scale(.5)
         ]);
+        add([
+          rect(500+200+200,50),
+          area(),
+          anchor("center"),
+          pos(500+25, 500-300),
+          z(19),
+          color(242, 140, 40),
+          "alertPop"
+
+        ])
 
         setTimeout(() => {
           destroyAll("alertPop");
-        }, 5000);
+        }, 2000);
         
 
 
