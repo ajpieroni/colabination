7d08771c6822a8ddb912dfd3c7e43836333c9a6d
Alex <akp42@duke.edu>
2024-01-25 12:02:03 -0500

    reconfigured labels

--- FrontEnd/utils/Craft.js
+++ FrontEnd/utils/Craft.js
@@ -293,7 +293,7 @@ export function openCraftWindow(craftState, inventoryState, toolState) {
   // Add tool text object
   add([
     text(toolDisplay, { size: 24 }),
-    pos(100 + 500, 100 + 50),
+    pos(100 + 500+50, 100 + 50),
     color(255, 255, 255),
     z(500),
     "craftingitem",
@@ -307,8 +307,15 @@ export function openCraftWindow(craftState, inventoryState, toolState) {
     "craft",
   ]);
   add([
+    text("Press [ Q ] To Remove Items", { size: 16 }),
+    pos(100 + 500 - 50+50, 100 + 50 + 500-100+50),
+    color(255, 255, 255),
+    z(500),
+    "craft",
+  ]);
+  add([
     text("Press [ Enter ] To Add Items", { size: 16 }),
-    pos(100 + 500, 100 + 50 + 100 - 50),
+    pos(100 + 500 - 50+50, 100 + 50 + 500-100+50-50),
     color(255, 255, 255),
     z(500),
     "craft",
