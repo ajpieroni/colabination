4170fee745fd803b7199d56db32a6cd343351ce0
Alex <akp42@duke.edu>
2024-02-19 11:42:09 -0500

    index fix

--- FrontEnd/utils/DocuStation.js
+++ FrontEnd/utils/DocuStation.js
@@ -80,7 +80,7 @@ export function showFinalItems(inventoryState, craftState) {
     const selected = add([
       rect(70, 70),
       pos(currentX, currentY),
-      z(19),
+      z(20),
       color(255, 255, 255),
       "selected",
       "final"
@@ -101,7 +101,7 @@ export function showFinalItems(inventoryState, craftState) {
 
       const finalItem = add([
         pos(currentX, currentY),
-        z(11),
+        z(50),
         sprite(`${item}`),
         "final",
         { itemKey: item },
@@ -115,54 +115,54 @@ export function showFinalItems(inventoryState, craftState) {
   console.log(inventoryState.areFinal);
 
   // Add items to documentation station
-  for (let i = 0; i < inventoryState.areFinal.length; i++) {
-    const item = inventoryState.areFinal[i];
-    if (item) {
-      let itemText = item.charAt(0).toUpperCase() + item.slice(1);
-      let resultDisplay = itemText
-        // space
-        .replace(/([A-Z])/g, " $1")
-        //trim
-        .split(" ")
-        .map(
-          (word) => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()
-        )
-        .join(" ");
-      const selectedText = add([
-        "finalText",
-        text(resultDisplay + "!!!", {
-          size: 24,
-          outline: 4,
-          color: (0, 0, 0),
-        }),
-        area(),
-        anchor("center"),
-        pos(325, 625),
-        z(20),
-        "final",
-      ]);
+  // for (let i = 0; i < inventoryState.areFinal.length; i++) {
+  //   const item = inventoryState.areFinal[i];
+  //   if (item) {
+  //     let itemText = item.charAt(0).toUpperCase() + item.slice(1);
+  //     let resultDisplay = itemText
+  //       // space
+  //       .replace(/([A-Z])/g, " $1")
+  //       //trim
+  //       .split(" ")
+  //       .map(
+  //         (word) => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()
+  //       )
+  //       .join(" ");
+  //     const selectedText = add([
+  //       "finalText",
+  //       text(resultDisplay + "!!!", {
+  //         size: 24,
+  //         outline: 4,
+  //         color: (0, 0, 0),
+  //       }),
+  //       area(),
+  //       anchor("center"),
+  //       pos(325, 625),
+  //       z(20),
+  //       "final",
+  //     ]);
 
-      // const itemKey = item.itemKey;
-      // starts a new line
+  //     // const itemKey = item.itemKey;
+  //     // starts a new line
 
-      if (currRow === 3) {
-        currentY += item.height + 50;
-        currentX = startX;
-        currRow = 0;
-      }
+  //     if (currRow === 3) {
+  //       currentY += item.height + 50;
+  //       currentX = startX;
+  //       currRow = 0;
+  //     }
 
-      const finalItem = add([
-        pos(currentX, currentY),
-        z(11),
-        sprite(`${item}`),
-        "final",
-        { itemKey: item },
-      ]);
+  //     const finalItem = add([
+  //       pos(currentX, currentY),
+  //       z(11),
+  //       sprite(`${item}`),
+  //       "final",
+  //       { itemKey: item },
+  //     ]);
 
-      currRow++;
-      currentX += 100;
-    }
-  }
+  //     currRow++;
+  //     currentX += 100;
+  //   }
+  // }
 }
 
 export function closeDocumentationStation() {
