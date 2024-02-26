4e3d80be321a8e000fecac43ec9c434883d3b7da
Alex <akp42@duke.edu>
2024-02-26 10:32:00 -0500

    scaffolded rest of materials

--- FrontEnd/utils/Tools.js
+++ FrontEnd/utils/Tools.js
@@ -20,19 +20,20 @@ export default function Tools(){
     
   ]);
   
-  // const scissors = add([
-  //   rect(block_size * 1.65, block_size * 1.15),
-  //   color(256, 0, 0),
-  //   area(),
-  //   body({ isStatic: true }),
-  //   pos(260, 260),
-  //   z(1),
-  //   "handTools",
-  //   "tool",
-  //   { toolKey: "scissors" },
-  //   { access: false },
-  //   { toolId: 2 },
-  // ]);
+  const scissors = add([
+    rect(block_size * 1.65, block_size * 1.15),
+    // make the color green
+    color(0, 256, 0),
+    area(),
+    body({ isStatic: true }),
+    pos(140+300, 40-20+10),
+    z(1),
+    "handTools",
+    "tool",
+    { toolKey: "scissors" },
+    { access: false },
+    { toolId: 2 },
+  ]);
   // const leatherTools = add([
   //   rect(block_size * 1.65, block_size * 4),
   //   color(256, 0, 0),
@@ -99,46 +100,46 @@ export default function Tools(){
     { access: false },
   ]);
   // !TODO: tools to add
-  // const cricut = add([
-  //   rect(block_size * 2.25, block_size),
-  //   "tool",
-  //   "cricut vinyl cutter",
-  //   // sprite("cricut"),
-  //   area(),
-  //   color(0, 0, 255),
-  //   pos(330, 710),
-  //   z(1),
-  //   body({ isStatic: true }),
-  //   { buildNoBlueprint: false },
-  //   { toolKey: "cricut vinyl cutter" },
-  //   { access: false },
-  //   { toolId: 8 },
-  // ]);
-  // const sewingMachine = add([
-  //   rect(block_size * 2.5, block_size),
-  //   color(255, 0, 0),
-  //   area(),
-  //   body({ isStatic: true }),
-  //   pos(500, 710),
-  //   z(1),
-  //   "printer",
-  //   "tool",
-  //   { toolKey: "sewing machine" },
-  //   { access: false },
-  //   { toolId: 11 },
-  // ]);
-  // const solderingStation = add([
-  //   rect(block_size * 2.25, block_size),
-  //   color(255, 0, 0),
-  //   area(),
-  //   body({ isStatic: true }),
-  //   pos(140, 40),
-  //   z(1),
-  //   "printer",
-  //   "tool",
-  //   { toolKey: "solderingStation" },
-  //   { access: false },
-  //   { toolId: 7 },
+  const cricut = add([
+    rect(block_size * 2.25, block_size),
+    "tool",
+    "cricut vinyl cutter",
+    // sprite("cricut"),
+    area(),
+    color(0, 0, 255),
+    pos(330-100, 710),
+    z(1),
+    body({ isStatic: true }),
+    { buildNoBlueprint: false },
+    { toolKey: "cricut vinyl cutter" },
+    { access: false },
+    { toolId: 8 },
+  ]);
+  const sewingMachine = add([
+    rect(block_size * 2.5, block_size),
+    color(255, 0, 0),
+    area(),
+    body({ isStatic: true }),
+    pos(500, 710),
+    z(1),
+    "printer",
+    "tool",
+    { toolKey: "sewing machine" },
+    { access: false },
+    { toolId: 11 },
+  ]);
+  const solderingStation = add([
+    rect(block_size * 2.25, block_size),
+    color(255, 0, 0),
+    area(),
+    body({ isStatic: true }),
+    pos(140, 40),
+    z(1),
+    "printer",
+    "tool",
+    { toolKey: "solderingStation" },
+    { access: false },
+    { toolId: 7 },
     
-  // ]);
+  ]);
 }
